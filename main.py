c87cd455acbeb5d32d981dbbef8e8bc36a4e7ec6
obfv-valiente <valiente.kr@obmontessori.edu.ph>
2026-09-15 20:06:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from pyscript import document, display
 
+subtotal = (float(prod1.value) * prod1.checked + float (prod2.value) * prod2.checked + float (prod3.value) * prod3.checked + float (prod4.value) * prod4.checked + float (prod5.value) * prod5.checked )
 
-def create_order(e):
-       document.getElementById("output1").innerHTML="" #clears previous result
 
-num1 = float(document.getElementById("input1").value) #get input value 
-num2 = float(document.getElementById("input2").value) #get input value
-num3 = float(document.getElementById("input3").value) #get input value
-num4 = float(document.getElementById("input4").value) #get input value
-num5 = float(document.getElementById("input5").value) #get input value
+display(f"Subtotal:{Subtotal}.", target="output1")
+display(f"Tax:{tax}.",target="output1",append=True)
+display(f"Total{total}.", target="output1", append=True)
+display("Thank you for your order", target="output1", append= True)
 
-result = num1 + num2 + num3 + num4 + num5 #use's the input value from the above to calculate
 
-display(f"Subtotal: {result}", target="output1")
 
-subtotal = (float(prod1))
+            
